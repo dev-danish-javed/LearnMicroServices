32c4a5551de308cb59072a3b4bff973f97e6a9af
Danish Javed <danishjaved636@gmail.com>
2023-04-24 11:56:44 +0530

    refresh not working

--- address/src/main/java/com/microservice/address/controllers/AddressController.java
+++ address/src/main/java/com/microservice/address/controllers/AddressController.java
@@ -5,15 +5,28 @@ import com.microservice.address.models.AddressRequest;
 import com.microservice.address.models.AddressResponse;
 import com.microservice.address.services.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RefreshScope
 @RequestMapping("/address")
 public class AddressController {
     @Autowired
     AddressService addressService;
+
+    @Value("${application.profile}")
+    private String profile;
+
+    @Value("${dev.specific}")
+    private String devSpecific;
+
+    @Value("${prod.specific}")
+    private String prodSpecific;
 
     @PostMapping
     public ResponseEntity<AddressResponse> addAddress(@RequestBody AddressRequest address) {
@@ -28,5 +41,12 @@ public class AddressController {
         System.out.println("I am address-service");
         AddressDto addressDto = addressService.getAddressById(id);
         return addressDto != null ? new ResponseEntity<>(new AddressResponse(addressDto), HttpStatus.OK) : new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return "profile : " + profile + "\n" +
+                "devSpecific : " + devSpecific + "\n" +
+                "prodSpecific : " + prodSpecific + "\n";
     }
 }
